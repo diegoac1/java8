c5e4a40fce684f89a9707e434104792a3a0b54cf
Diego <diegoacosta005@gmail.com>
2022-04-28 19:55:41 -0300

    first commit

---
+++ src/OrdenaString.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class OrdenaString {
+
+	public static void main(String[] args) {
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("alura online");
+		palavras.add("editora casa do código");
+		palavras.add("caelum");
+		System.out.println(palavras);
+		palavras.sort((s1, s2) -> {
+				if(s1.length() < s2.length())
+					return -1;
+				if(s1.length() > s2.length())
+					return 1;
+				return 0;
+			
+		});
+		
+		//palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String::length));
+		
+		Function<String, Integer> funcao = String::length;
+		Function<String, Integer> funcao2 = s -> s.length();
+		
+		Comparator<String> comparador = Comparator.comparing(funcao);
+		palavras.sort(comparador);
+		
+		
+		System.out.println(palavras);
+
+		Consumer<String> impressor = System.out::println;
+		palavras.forEach(impressor);
+		
+		palavras.forEach(s -> System.out.println(s));
+		palavras.forEach(System.out::println);
+	}
+}
